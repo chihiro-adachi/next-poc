9e1a31f0961317c0aebd76fd8b5c8d2545f52fa9
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-06-19 11:57:19 +0900

    Symfony\Component\Security\Core\User\UserInterface

--- src/application/Eccube/Entity/Customer.php
+++ src/application/Eccube/Entity/Customer.php
@@ -14,7 +14,7 @@
 namespace Eccube\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Eccube\Security\Core\User\UserInterface;
 
 if (!class_exists('\Eccube\Entity\Customer')) {
     /**
