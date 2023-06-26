510b5ec1a4e22a71199697e0e0a59cedb81e92ea
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-06-27 07:36:44 +0900

    Eccube\ORM\Collections\ArrayCollectionを使うように修正

--- src/application/Eccube/Entity/Cart.php
+++ src/application/Eccube/Entity/Cart.php
@@ -13,7 +13,7 @@
 
 namespace Eccube\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
+use Eccube\ORM\Collections\ArrayCollection;
 use Eccube\ORM\Mapping as ORM;
 use Eccube\Service\PurchaseFlow\InvalidItemException;
 use Eccube\Service\PurchaseFlow\ItemCollection;
